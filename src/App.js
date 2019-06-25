c90e506d418a3ead240c7ab0cf262ae26b65be98
Tonzzu <t.o.ruusunen@gmail.com>
2019-06-25 19:44:40 +0300

    First commit"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bgColor: "#fff"
+    };
+  }
+
+  bgColorPicker(e) {
+    this.setState({
+      bgColor: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" style={{ backgroundColor: this.state.bgColor }}>
+        <div className="colorBox">
+          <h2>React</h2>
+          {/* <h1 style={{ color: this.state.bgColor }}>React Color Picker</h1> */}
+          <div className="Title">
+            <h2 className="c">C</h2>
+            <h2 className="o">O</h2>
+            <h2 className="l">L</h2>
+            <h2 className="o">O</h2>
+            <h2 className="r">R</h2>
+          </div>
+          <h2> Picker</h2>
+          <label>
+            {/* Pick a color */}
+            <input type="color" onChange={e => this.bgColorPicker(e)} />
+          </label>
+          <p>HEX Color code:</p>
+          <p style={{ fontWeight: "bold", fontSize: "22px" }}>
+            {this.state.bgColor.toLocaleUpperCase()}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
